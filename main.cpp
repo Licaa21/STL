96449d54f8d893f57e93d9ce54959ba9e4157ef5
Licaa21 <lica.albert.u9a@student.ucv.ro>
2024-11-12 17:56:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,70 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+struct problem
+{
+    string id;
+    string speciality;
+};
+
+struct doctor
+{
+    string id;
+    string speciality;
+};
 int main()
 {
     ifstream inFile("input.txt");
 
     int no_problems, no_doctors;
-    string name, speciality;
     
     inFile >> no_problems;
-
+    string name, speciality;
+    std::vector<problem> problems;
     for (int i = 0; i < no_problems; i++)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        problem n;
+        inFile >> n.id;
+        inFile >> n.speciality;
+        problems.push_back(n);
+      
     }
 
     inFile >> no_doctors;
 
+    std::vector<doctor> doctors;
+
     for (int i = 0; i < no_doctors; i++)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
-    }
+        doctor doc;
 
+        inFile >> doc.id;
+        inFile >> doc.speciality;
+        
+        doctors.push_back(doc);
+    }
+    for (auto problem: problems)
+    {
+        bool found = 0;
+        for (auto doctor: doctors)
+        {
+            if (problem.speciality == doctor.speciality)
+            {
+                found = 1;
+                break;
+            }
+            
+        }
+        if (found == 0)
+            cout << problem.id << " " << "Repsins" << endl;
+        else
+            cout << problem.id << " " << "Acceptat"<< endl;
+
+    }
+    
     return 0;
 }
